0faa1f02e3db255e79b8ac150647c30a4b7f1c53
Ícaro Jerry <icaro.jerry@gmail.com>
2015-11-28 00:20:01 -0300

    Updating authentication feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     mount_uploader :picture, PictureUploader
+
     validates :username, :name, :email, presence: true
     validates :name, length: { maximum: 60 }
     validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
     validates :username, uniqueness: { case_sensitive: false }, format: { with: /\A[-a-z0-9]+\Z/i, on: :create }
+
+    def login=(login)
+      @login = login
+    end
+
+   def login
+     @login || self.username || self.email
+   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
