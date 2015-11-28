1cde56cdbae0673901b7b3b438f4bdeee8f04a9f
Ícaro Jerry <icaro.jerry@gmail.com>
2015-11-27 21:07:22 -0300

    Includes new user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     mount_uploader :picture, PictureUploader
+    validates :username, :name, :email, presence: true
+    validates :name, length: { maximum: 60 }
+    validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+    validates :username, uniqueness: { case_sensitive: false }, format: { with: /\A[-a-z0-9]+\Z/i, on: :create }
 end
